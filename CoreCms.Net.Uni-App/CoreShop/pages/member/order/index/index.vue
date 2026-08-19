81470d2747b6bbc7e075088c03f2cb74f1c64bd0
Ricardo <120153575+RamuneSodaa@users.noreply.github.com>
2026-08-19 03:55:14 -0400

    feat(seafood): render live reservations in customer order page

--- CoreCms.Net.Uni-App/CoreShop/pages/member/order/index/index.vue
+++ CoreCms.Net.Uni-App/CoreShop/pages/member/order/index/index.vue
@@ -1,76 +1,85 @@
-﻿<template>
+<template>
     <view>
         <u-toast ref="uToast" /><u-no-network></u-no-network>
-        <u-navbar title="订单列表"></u-navbar>
-        <view class="orderWrap">
-            <view class="u-padding-0">
-                <u-subsection :list="tabs" :current="current" :animation="true" @change="onClickItem" active-color="#ff9900" mode="button"></u-subsection>
-            </view>
 
-            <view class="page-box" v-if="listData.length > 0">
-                <view class="orderList" v-for="(order, orderIndex) in listData" :key="orderIndex">
-                    <view class="top" @click="goOrderDetail(order.orderId)">
-                        <view class="left">
-                            <u-icon name="home" :size="30" color="rgb(94,94,94)"></u-icon>
-                            <view class="store">订单号 : {{order.orderId}}</view>
-                            <u-icon name="arrow-right" color="rgb(203,203,203)" :size="26"></u-icon>
-                        </view>
-                        <view class="right">{{ order.orderStatusName }}</view>
-                    </view>
-                    <view class="item" v-for="(goods, indexGoods) in order.items" :key="indexGoods">
-                        <view class="left"><image :src="goods.imageUrl && goods.imageUrl!='null' ?  goods.imageUrl : '/static/images/common/empty-banner.png'" mode="aspectFill"></image></view>
-                        <view class="content">
-                            <view class="title u-line-2">{{goods.name}}</view>
-                            <view class="type">{{goods.addon}}</view>
-                            <view class="delivery-time">下单时间：{{ $u.timeFormat(goods.createTime, 'yyyy-mm-dd hh:MM:ss') }}</view>
-                        </view>
-                        <view class="right">
-                            <view class="price">
-                                ￥{{ goods.price }}
-                            </view>
-                            <view class="number">x{{ goods.nums }}</view>
-                        </view>
-                    </view>
-                    <view class="total">
-                        共{{ order.items.length }}件商品 合计:
-                        <text class="total-price">
-                            ￥{{ order.orderAmount }}
-                        </text>
-                    </view>
-                    <view class="bottom">
-                        <view class="more">
-                            <u-tag :text="order.typeText" mode="light" />
-                        </view>
-                        <view class="u-flex">
-                            <view class='logistics coreshop-btn'  @click="goOrderDetail(order.orderId)">查看详情</view>
-                            <view class='coreshop-btn exchange'  v-if="order.status === 1 && order.payStatus === 1" @click="goToPay(order.orderId)">立即支付</view>
-                            <view class='coreshop-btn exchange'  v-if="order.status === 1 && order.payStatus >= 2 && order.shipStatus >= 3 && order.confirmStatus === 1" @click="tackDelivery(current,orderIndex)">确认收货</view>
-                            <view class='evaluate coreshop-btn'  v-if="order.status === 1 && order.payStatus >= 2 && order.shipStatus >= 3 && order.confirmStatus >= 2 && order.isComment === false" @click="toEvaluate(order.orderId)">立即评价</view>
-                        </view>
-                    </view>
+        <template v-if="seafoodMode">
+            <u-navbar title="我的预订"></u-navbar>
+            <seafood-my-reservations ref="seafoodReservations"></seafood-my-reservations>
+        </template>
+
+        <template v-else>
+            <u-navbar title="订单列表"></u-navbar>
+            <view class="orderWrap">
+                <view class="u-padding-0">
+                    <u-subsection :list="tabs" :current="current" :animation="true" @change="onClickItem" active-color="#ff9900" mode="button"></u-subsection>
                 </view>
-                <u-loadmore :status="loadStatus" bgColor="#f2f2f2"></u-loadmore>
+
+                <view class="page-box" v-if="listData.length > 0">
+                    <view class="orderList" v-for="(order, orderIndex) in listData" :key="orderIndex">
+                        <view class="top" @click="goOrderDetail(order.orderId)">
+                            <view class="left">
+                                <u-icon name="home" :size="30" color="rgb(94,94,94)"></u-icon>
+                                <view class="store">订单号 : {{order.orderId}}</view>
+                                <u-icon name="arrow-right" color="rgb(203,203,203)" :size="26"></u-icon>
+                            </view>
+                            <view class="right">{{ order.orderStatusName }}</view>
+                        </view>
+                        <view class="item" v-for="(goods, indexGoods) in order.items" :key="indexGoods">
+                            <view class="left"><image :src="goods.imageUrl && goods.imageUrl!='null' ? goods.imageUrl : '/static/images/common/empty-banner.png'" mode="aspectFill"></image></view>
+                            <view class="content">
+                                <view class="title u-line-2">{{goods.name}}</view>
+                                <view class="type">{{goods.addon}}</view>
+                                <view class="delivery-time">下单时间：{{ $u.timeFormat(goods.createTime, 'yyyy-mm-dd hh:MM:ss') }}</view>
+                            </view>
+                            <view class="right">
+                                <view class="price">
+                                    ￥{{ goods.price }}
+                                </view>
+                                <view class="number">x{{ goods.nums }}</view>
+                            </view>
+                        </view>
+                        <view class="total">
+                            共{{ order.items.length }}件商品 合计:
+                            <text class="total-price">
+                                ￥{{ order.orderAmount }}
+                            </text>
+                        </view>
+                        <view class="bottom">
+                            <view class="more">
+                                <u-tag :text="order.typeText" mode="light" />
+                            </view>
+                            <view class="u-flex">
+                                <view class='logistics coreshop-btn' @click="goOrderDetail(order.orderId)">查看详情</view>
+                                <view class='coreshop-btn exchange' v-if="order.status === 1 && order.payStatus === 1" @click="goToPay(order.orderId)">立即支付</view>
+                                <view class='coreshop-btn exchange' v-if="order.status === 1 && order.payStatus >= 2 && order.shipStatus >= 3 && order.confirmStatus === 1" @click="tackDelivery(current,orderIndex)">确认收货</view>
+                                <view class='evaluate coreshop-btn' v-if="order.status === 1 && order.payStatus >= 2 && order.shipStatus >= 3 && order.confirmStatus >= 2 && order.isComment === false" @click="toEvaluate(order.orderId)">立即评价</view>
+                            </view>
+                        </view>
+                    </view>
+                    <u-loadmore :status="loadStatus" bgColor="#f2f2f2"></u-loadmore>
+                </view>
+
+                <view class="coreshop-emptybox" v-else>
+                    <u-empty :src="$globalConstVars.apiFilesUrl+'/static/images/empty/order.png'" icon-size="300" text="您还没有相关类别的订单" mode="list"></u-empty>
+                    <navigator class="coreshop-btn" url="/pages/category/index/index" open-type="switchTab">随便逛逛</navigator>
+                </view>
             </view>
-
-
-            <!-- 无数据时默认显示 -->
-            <view class="coreshop-emptybox" v-else>
-                <u-empty :src="$globalConstVars.apiFilesUrl+'/static/images/empty/order.png'" icon-size="300" text="您还没有相关类别的订单" mode="list"></u-empty>
-                <navigator class="coreshop-btn" url="/pages/category/index/index"  open-type="switchTab">随便逛逛</navigator>
-            </view>
-
-
-        </view>
+        </template>
     </view>
-
-
 </template>
+
 <script>
     import { orders, goods } from '@/common/mixins/mixinsHelper.js'
+    import SeafoodMyReservations from '@/components/seafood/MyReservations.vue'
+
     export default {
+        components: {
+            SeafoodMyReservations
+        },
         mixins: [orders, goods],
         data() {
             return {
+                seafoodMode: false,
                 tabs: ['全部', '待付款', '待发货', '待收货', '待评价'],
                 items: ['未使用', '已使用', '已失效'],
                 current: 0,
@@ -84,10 +93,15 @@
                     loading: '努力加载中',
                     nomore: '实在没有了'
                 },
-                status: [0, 1, 2, 3, 4],// 订单状态 0全部 1待付款 2待发货 3待收货 4待评价
+                status: [0, 1, 2, 3, 4],
             }
         },
         onLoad: function (option) {
+            this.seafoodMode = !!(option && String(option.seafood || '') === '1');
+            if (this.seafoodMode) {
+                return;
+            }
+
             var currentIndex = option.swiperCurrentIndexId;
             if (currentIndex) {
                 this.current = currentIndex;
@@ -95,14 +109,17 @@
             this.getOrders();
         },
         onReachBottom() {
+            if (this.seafoodMode) return;
             if (this.loadStatus === 'loadmore') {
                 this.getOrders();
             }
         },
         onShow() {
+            if (this.seafoodMode && this.$refs.seafoodReservations) {
+                this.$refs.seafoodReservations.refreshOrders(false);
+            }
         },
         methods: {
-            // tab点击切换
             onClickItem(index) {
                 if (this.current !== index) {
                     this.current = index;
@@ -111,8 +128,8 @@
                     this.getOrders();
                 }
             },
-            //获取列表
             getOrders() {
+                if (this.seafoodMode) return;
                 this.loadStatus = 'loading'
                 let data = {
                     page: this.page,
@@ -137,18 +154,15 @@
                 });
             },
             removeorder: function (oid) {
-                //console.log(oid);
                 uni.showModal({
                     title: '确认提醒',
                     content: '您确定要移除订单 [ ' + oid + ' ] 吗？',
                     success: function (e) {
                         if (e.confirm) {
-                            //自行完善删除代码
                         }
                     }
                 });
             },
-            // 确认收货
             tackDelivery(index, orderIndex) {
                 let _this = this;
                 this.$common.modelShow('提示', '确认执行收货操作吗?', () => {
@@ -172,7 +186,6 @@
                     })
                 })
             },
-            // 订单状态统一在这处理
             formatOrderStatus(orderList) {
                 orderList.forEach(item => {
                     switch (item.status) {
@@ -209,7 +222,6 @@
         },
     }
 </script>
-
 
 <style lang="scss">
     /* #ifndef H5 */
